c314bfd1f55088de4508c9dd62f2395cfcaed74a
Fenomenon199 <afeldman2025@kehillahstudent.org>
2024-09-30 09:21:12 -0700

    Project 1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import turtle
-
-turtle.forward(100)
+#square  
+for i in range (6): 
+    turtle.forward(9)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50) 
+turtle.exitonclick()
